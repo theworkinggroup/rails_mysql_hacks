464216398c257abb482cb39e4026a35872cb3321
tadman <github@tadman.ca>
2009-04-08 11:52:32 -0400

    Add options[:having] to add_group! call

--- lib/mysql_hacks/extensions/base.rb
+++ lib/mysql_hacks/extensions/base.rb
@@ -88,7 +88,7 @@ protected
     add_conditions!(sql, options[:conditions], scope)
     add_limited_ids_condition!(sql, options, join_dependency) if !using_limitable_reflections?(join_dependency.reflections) && ((scope && scope[:limit]) || options[:limit])
 
-    add_group!(sql, options[:group], scope)
+    add_group!(sql, options[:group], options[:having], scope)
     add_order!(sql, options[:order], scope)
     add_limit!(sql, options, scope) if using_limitable_reflections?(join_dependency.reflections)
     add_lock!(sql, options, scope)
